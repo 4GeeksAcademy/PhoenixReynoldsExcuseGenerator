eacfbbb35c3b6f83467043cd7fcefc0a8b371efb
Phoenix Reynolds <phoenix.b.reynolds@gmail.com>
2025-05-27 23:16:30 +0000

    FinalProject

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse()
+    })
+  const generateExcuse = () => {
+    let who = ['The dog', 'My grandma', 'The mailman', 'My wife\'s boyfriend'];
+    let action = ['ate', 'granted a new, eldritch form of evolution to', 'disintigrated', 'killed'];
+    let what = ['my father', 'my cellular device', 'the cybertruck'];
+    let when = ['before the ritual', 'when I was big snoozin', 'while I was taking my daily scream', 'during my ascension', 'while I was jorkin'];
+
+    let whoIndex = Math.floor(Math.random() * who.length)
+    let actionIndex = Math.floor(Math.random() * action.length)
+    let whatIndex = Math.floor(Math.random() * what.length)
+    let whenIndex = Math.floor(Math.random() * when.length)
+
+    return (who[whoIndex] + " " + action[actionIndex] + " " + what[whatIndex] + " " + when[whenIndex]
+    )
+  }
+}
